5ce4c43da6f506fc7a38bfed3935e7c1158018e7
Savindu Wijesinghe <savinduwijesinghe95@gmail.com>
2026-01-19 23:39:04 +0530

    Update next.config.ts

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,18 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com', // Unsplash Images වලට අවසර දීම
+        hostname: 'images.unsplash.com', 
       },
       {
         protocol: 'https',
-        hostname: 'randomuser.me', // Random User Images වලට අවසර දීම
+        hostname: 'randomuser.me',
       },
       {
         protocol: 'https',
-        hostname: 'cdn.sanity.io', // Sanity Images (අනාගතය සඳහා)
+        hostname: 'cdn.sanity.io', 
       }
     ],
   },
